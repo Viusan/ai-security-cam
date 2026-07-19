3c6ca14c0e32af963970f20ac3e2abbaeee10468
viusa <viusansantos@gmail.com>
2026-07-20 00:42:39 +0200

    update bot to send photo alerts

--- test_bot.py
+++ test_bot.py
@@ -10,6 +10,11 @@ CHAT_ID = os.getenv("CHAT_ID")
 
 async def send_test_message():
     bot = Bot(token=TOKEN)
-    await bot.send_message(chat_id=CHAT_ID, text="Security cam bot is working")
+    with open("test_image.jpg", "rb") as photo:
+        await bot.send_photo(
+            chat_id=CHAT_ID,
+            photo=photo,
+            caption="Test alert: object detected"
+        )
 
 asyncio.run(send_test_message())
